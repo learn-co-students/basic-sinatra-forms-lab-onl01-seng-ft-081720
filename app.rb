84d44e6a028140c57ac7e093255a4c1cfa62b18b
atBeatty <adam.t.beatt@gmail.com>
2020-09-28 13:37:11 -0700

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,17 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do 
 
+        erb :newteam
+    end
+
+    post '/newteam' do
+        
+        erb :team
+
+    end
+
+
+    
 end
