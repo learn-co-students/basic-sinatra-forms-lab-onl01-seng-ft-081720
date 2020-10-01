002ae35bdb554586ec027e7a0fb2954ed2b1c1b9
mollymoog <mmkover@gmail.com>
2020-10-01 08:24:22 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,22 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do
+        erb :newteam
+    end
+
+    post '/newteam' do
+        puts params
+       @team_name = params[:name]
+       @coach = params[:coach]
+       @point_guard = params[:pg]
+       @shooting_guard = params[:sg]
+       @power_fwd = params[:pf]
+       @small_fwd = params[:sf]
+       @center = params[:c]
+
+        erb :team
+    end
+
 
 end
